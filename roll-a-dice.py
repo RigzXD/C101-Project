f2c47abe774893d6dd2b5dc2300070f0abe52734
RigzXD <120970458+RigzXD@users.noreply.github.com>
2024-01-07 10:13:09 -0800

    Add files via upload

--- roll-a-dice.py
+++ roll-a-dice.py
@@ -1,7 +1,44 @@
 import random
-random = input("Do you want to roll a die? ")
 
-def response(random):
-    while response == "y":
-        no = random.randint(1,6)
-        
+response = "y"
+while response == "y":
+    no = random.randint(1,6)
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")   
+    if no == 2:
+        print("[-----]")
+        print("[    0]")
+        print("[     ]")
+        print("[0    ]")
+        print("[-----]")
+    if no == 3:
+        print("[-----]")
+        print("[    0]")
+        print("[  0  ]")
+        print("[0    ]")
+        print("[-----]")
+    if no == 4:
+        print("[-----]")
+        print("[0   0]")
+        print("[     ]")
+        print("[0   0]")
+        print("[-----]")
+    if no == 5:
+        print("[-----]")
+        print("[0   0]")
+        print("[  0  ]")
+        print("[0   0]")
+        print("[-----]")
+    if no == 6:
+        print("[-----]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[0   0]")
+        print("[-----]")
+
+    response = input("Press y to roll again: ")
+    print("\n")
